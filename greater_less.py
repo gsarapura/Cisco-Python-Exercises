cab868b6be1a00b939c59ea02f1fb7e71e216d91
gustavolens <95143213+gustavolens@users.noreply.github.com>
2022-05-28 20:45:00 -0300

    Update greater_less.py

--- greater_less.py
+++ greater_less.py
@@ -1,6 +1,6 @@
 # Take a number and print True if it is equal or grater than 100 or False if it is less than 100.
-# It is a very simple program but I could not do it without and if statement.
-# Remainder: keep it simple!
+# It is a very simple program but I could not do it without an if statement.
+# Reminder: keep it simple!
 
 n = int(input("Type any number, please: "))
 print(n >= 100)
