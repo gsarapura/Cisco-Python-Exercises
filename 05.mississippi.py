c820b507a18f7a86aefc9b19384ec95e9d697c6c
Gustavo Sarapura <gustavo.sarapura7@gmail.com>
2022-06-06 13:24:22 -0300

    Add Mississippi exercise Take into account the concept and use of module and method

---
+++ 05.mississippi.py
@@ -0,0 +1,9 @@
+import time
+
+for i in range(1, 6):    # Loop that longs 5 sec.
+    print(i, "Mississippi")
+    # time.sleep (1), later we'll learn about modules (time) and methods(sleep)
+    time.sleep(1)
+
+# Final message
+print("Ready or not, here I go!")
