661c3306d9bf2d49bf069c3f267dc75219646cd0
Gustavo Sarapura <gustavo.sarapura7@gmail.com>
2022-06-23 18:11:05 -0300

    Correct the range of assert in days_in_month

--- 18.days_in_month.py
+++ 18.days_in_month.py
@@ -15,7 +15,7 @@ def is_year_leap(year):
 def days_in_month(year, month):
     days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     
-    assert month >= 0 and month <=11, 'Month is out of range'
+    assert month >= 0 and month <=12, 'Month is out of range'
     if is_year_leap(year):
         days_in_month[1] = 29
     return days_in_month[month - 1]
