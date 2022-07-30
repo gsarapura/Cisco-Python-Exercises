80ea7f8ff9f91f3c40bc4b76af8dd53dc9b5cbf7
Gustavs <gustavo.sarapura7@gmail.com>
2022-07-30 17:39:24 -0300

    Add module example

---
+++ Python-2/1.import_modules.py
@@ -0,0 +1,15 @@
+import math
+
+# There is not any conflicts since there are two namespaces. 
+# One in which I created sin() and pi and other which was imported.
+
+def sin(x):
+    if 2 * x == pi:
+        return 1
+    else:
+        return None
+
+pi = 3.14
+
+print(sin(pi/2)) 
+print(math.sin(math.pi/2))
