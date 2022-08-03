a87e802c58122ae8abc6dae2fb9c3d7d7e0c5fd1
Gustavs <gustavo.sarapura7@gmail.com>
2022-08-03 15:06:07 -0300

    Update module folder

--- Python-2/module/module.py
+++ Python-2/module/module.py
@@ -1,6 +1,28 @@
-print("Esto es un módulo")
+#! /usr/bin/python3.10 => ./main.py on terminal. Remeber to chmod 764 main.py
+
+"""Ejemplo de módulo"""
+
+__counter = 0 # times this module has been called
+
+def sum1(the_list):
+    global __counter
+    __counter += 1
+    the_sum = 0
+    for element in the_list:
+        the_sum += element
+    return the_sum
+
+def prod1(the_list):
+    global __counter
+    __counter += 1
+    prod = 1
+    for element in the_list:
+        prod *= element
+    return prod
+    
 print(__name__)
-if __name__ == "__main__":
-    print("Estoy en un módulo")
-else:
-    print("Estoy en main") 
+if __name__ == "__name__": # to be interpreted only in module.py / "module" to be interpreted in main.py
+    print("Estoy en un módulo, pero puedo hacer unas cuentas.")
+    my_list = [i+1 for i in range(5)]
+    print(sum1(my_list) == 15)
+    print(prod1(my_list) == 120)
