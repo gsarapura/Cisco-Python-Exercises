3f8a302bd5e049dd371ed3005c93b1bad786824f
Gustavs <gustavo.sarapura7@gmail.com>
2022-08-04 12:14:21 -0300

    Organize packets and modules

---
+++ Python-2/programs/main.py
@@ -0,0 +1,13 @@
+# To work with "Archivo Zip Módulos_y_Paquetes"
+
+from sys import path 
+# it was needed to '//':
+path.append('/home/gustavs/repo/Cisco-Python-Exercises/Python-2/Archivo_ZIP_Módulos_y_Paquetes')
+# Check Path:
+#for name in path:
+    #print(name)
+
+# extra is a packet
+import extra.iota # Pylance gives error, but just add it to its path. Vscode tells you to add it.
+
+print(extra.iota.FunI())
