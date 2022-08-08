2b77a24e5c03280e46b9812edf4b64c6f394de31
Gustavs <gustavo.sarapura7@gmail.com>
2022-08-08 11:06:32 -0300

    Add numbers processor

---
+++ Python-2/12.numbers_processor.py
@@ -0,0 +1,14 @@
+#Procesador de Números.
+
+line = input("Ingresa una línea de números, sepáralos con espacios: ")
+# If empty, keep asking for numbers.
+while line == '':
+    line = input("Ingresa una línea de números, sepáralos con espacios: ")
+strings = line.split()
+total = 0
+try:
+    for substr in strings:
+        total += float(substr)
+    print("El total es:", total)
+except:
+    print(substr, "no es un numero.")
