c025e8d0a19dcb7e5db33ff587c46a300d647b02
Gustavs <gustavo.sarapura7@gmail.com>
2022-08-09 11:24:12 -0300

    Add prompt for min and max. It asks to enter min, max and prompt again, although min <= max.

--- Python-2/15.integers.py
+++ Python-2/15.integers.py
@@ -1,23 +1,26 @@
-def read_int(prompt, min, max):
+def read_int(min, max, prompt):
     """It prints whether the prompted integer is within the range specified.
     It asks the user to prompt again in case of AssertionError and ValueError.
-    
+
     Args:
         prompt: The integer to check.
         min (int): The minimum level.
         max (int): The maximum level.
     """
     try:
-        prompt = input(prompt)
-        assert int(prompt) >= min and int(prompt) <= max
+        min = int(input(min))
+        max = int(input(max))
+        assert min <= max, 'Min must be less or equal than max'
+        prompt = int(input(prompt))
+        assert prompt >= min and prompt <= max
         print("Yes, it is within the range specified.")
     except AssertionError:
         print("Error: prompt is not within the range specified.")
         # Is this recursiveness allowed?
-        read_int("Enter an integer value between -10 and 10: ", -10, 10)
+        read_int("Enter minimum level: ", "Enter maximum level: ", "Enter an integer value between minimum and maximum levels: ")
     except ValueError:
         print("Error: invalid prompt.")
         # Is this recursiveness allowed?
-        read_int("Enter an integer value between -10 and 10: ", -10, 10)
+        read_int("Enter minimum level: ", "Enter maximum level: ", "Enter an integer value between minimum and maximum levels: ")
 
-read_int("Enter an integer value between -10 and 10: ", -10, 10)
+read_int("Enter minimum level: ", "Enter maximum level: ", "Enter an integer value between minimum and maximum levels: ")
