c997188b59c649eeaf088b15da4e4de944e5a79e
Gustavs <gustavo.sarapura7@gmail.com>
2022-08-09 11:01:02 -0300

    Add prompt for integer to check

--- Python-2/15.integers.py
+++ Python-2/15.integers.py
@@ -1,23 +1,18 @@
 def read_int(prompt, min, max):
-    """Checks whether the prompt integer is within the range specified.
+    """Checks whether the prompted integer is within the range specified.
     Args:
         prompt: The integer to check.
         min (int): The minimum level.
         max (int): The maximum level.
-    Returns:
-        bool: True if the prompt integer is within the range specified.
     """
     try:
+        prompt = input(prompt)
         assert int(prompt) >= min and int(prompt) <= max
-        return True
+        print("Yes, it is within the range specified.")
     except AssertionError:
         print("Error: prompt is not within the range specified.")
     except ValueError:
         print("Error: invalid prompt.")
     
-v = read_int(1, -10, 10)
+v = read_int("Enter an integer value betwee -10 and 10: ", -10, 10)
 
-if v:
-    print("Yes, it is within the range specified.")
-else:
-    print("No, it is within the range specified.")
