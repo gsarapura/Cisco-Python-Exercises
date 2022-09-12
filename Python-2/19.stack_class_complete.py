b812aa3c7fa89b45b81ca3a92a20e17a1196a3af
Gustavs <gustavo.sarapura7@gmail.com>
2022-09-12 19:13:10 -0300

    Add theoretical class

--- Python-2/19.stack_class_complete.py
+++ Python-2/19.stack_class_complete.py
@@ -15,18 +15,29 @@ class Stack:
 # Define subclass that will sum all elements of stack:
 class AddingStack(Stack):
     def __init__(self):
-        Stack.__init__(self)
+        Stack.__init__(self) # In Python, you must explicitly invoke superclass constructor.
         self.__sum = 0
+    # Push method must add variable to sum and use push method from superclass.
+    # Besides, the only way to access stack_list is through superclass.
+    def push(self, val):
+        self.__sum += val
+        Stack.push(self, val)
+    # Pop method must remove variable from sum and use pop method from superclass.
+    def pop(self):
+        val = Stack.pop(self)
+        self.__sum -= val
+        return val
+    def get_sum(self):
+        return self.__sum
 
-little_stack = Stack()
-another_stack = Stack()
-funny_stack = Stack()
+stack_object = AddingStack()
 
-little_stack.push(1)
-another_stack.push(little_stack.pop() + 1)
-funny_stack.push(another_stack.pop() - 2)
+stack_object.push(2)
+stack_object.push(89)
+stack_object.push(233)
+stack_object.push(10)
+stack_object.push(41)
 
-print(funny_stack.pop())
-
-
-
+stack_object.pop()
+stack_object.pop()
+print("Total sum:", stack_object.get_sum())
