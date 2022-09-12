a8ff947ab61f16b127dac44afc0103783d5efb74
Gustavs <gustavo.sarapura7@gmail.com>
2022-09-12 19:53:32 -0300

    Add FIFO exercise

---
+++ Python-2/21.FIFO.py
@@ -0,0 +1,27 @@
+class QueueError(IndexError): # Eligir la clase base para la nueva excepción.
+    pass
+
+class Queue:
+    def __init__(self):
+        self.__stk = []
+
+    def put(self, elem):
+        self.__stk.insert(0, elem)
+
+    def get(self):
+        
+        elem = self.__stk[-1]
+        self.__stk.pop(-1)
+        return elem
+        
+
+que = Queue()
+que.put(1)
+que.put("perro")
+que.put(False)
+
+try:
+    for i in range(4):
+        print(que.get())
+except:
+    print("Error de Cola")
