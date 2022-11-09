21d7d1c09c5a51bce0deacff4b4471561cf5fef2
Gustavs <gustavo.sarapura7@gmail.com>
2022-11-09 17:47:38 -0300

    Add bitearrays

--- Python-2/46.bitearrays.py
+++ Python-2/46.bitearrays.py
@@ -1,7 +1,17 @@
+from os import strerror
+
 data = bytearray(10)
 
 for i in range(len(data)):
-    data[i] = 10 - i
+    data[i] = 10 + i
+
+try:
+    binary_file = open('file.bin', 'wb')
+    binary_file.write(data)
+    binary_file.close()
+except IOError as e:
+    print("E/S error:", strerror(e.errno))
 
 for b in data:
     print(hex(b))
+
